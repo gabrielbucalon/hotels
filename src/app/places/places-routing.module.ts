41e15996631e1ad18e5fafa31896d71a69b0ec12
Gabriel Bucalon <gabriel.bucalon@synvia.com.br>
2019-11-21 15:12:02 -0300

    <feat>: settings routers -> create service -> nagivation tabs

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -4,10 +4,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { PlacesPage } from './places.page';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'tabs', pathMatch: 'full' },
   {
-    path: '',
-    component: PlacesPage
-  }
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      { path: '', redirectTo: 'discover', pathMatch: 'full' },
+      {
+        path: 'discover',
+        loadChildren: () => import('./discover/discover.module').then(m => m.DiscoverPageModule)
+      },
+      {
+        path: 'offers',
+        loadChildren: () => import('./offers/offers.module').then(m => m.OffersPageModule)
+      }
+    ]
+  },
 ];
 
 @NgModule({
